6f3a4d03aaa6bbee23609d8a51bd1f7e7afbde2b
Ezequiel Flores <65667886+jef586@users.noreply.github.com>
2023-10-02 02:12:22 -0300

    Add files via upload

---
+++ vue-shims.d.ts
@@ -0,0 +1,7 @@
+// vue-shims.d.ts
+declare module "*.vue" {
+    import { defineComponent } from "vue";
+    const component: ReturnType<typeof defineComponent>;
+    export default component;
+  }
+  
